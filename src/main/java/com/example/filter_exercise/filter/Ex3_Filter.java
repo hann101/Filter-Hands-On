76151546899fb32dbc0f35b570120f9ec7b03bce
hann101 <hann20439@gmail.com>
2023-08-14 13:45:51 +0900

    추가 수정

--- src/main/java/com/example/filter_exercise/filter/Ex3_Filter.java
+++ src/main/java/com/example/filter_exercise/filter/Ex3_Filter.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 public class Ex3_Filter extends OncePerRequestFilter {
@@ -18,5 +19,12 @@ public class Ex3_Filter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("[Filter 3] OncePerRequestFilter 동작확인 [{}]", request.getAttribute("uuid") );
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String[] excludePath = {"/xss"};
+        String path = request.getRequestURI();
+        return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 }
